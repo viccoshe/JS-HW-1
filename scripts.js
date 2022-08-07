d6ceb4e5728292e7dc86be11efe243602558fcca
Victoria <viccoshe@gmail.com>
2022-08-07 11:06:05 +0300

    second commit

---
+++ scripts.js
@@ -0,0 +1,136 @@
+// Task 2
+
+let a1 = 5 % 3,
+    a2 = 3 % 5,
+    a3 = 5 + '3',
+    a4 = '5' - 3,
+    a5 = 75 + 'кг',
+    a6 = 785 * 653,
+    a7 = 100 / 25,
+    a8 = 0 * 0,
+    a9 = 0 / 2,
+    a10 = 89 / 0,
+    a11 = 98 + 2,
+    a12 = 5 - 98,
+    a13 = (8 + 56 * 4) / 5, 
+    a14 = (9 - 12) * 7 / (5 + 2), 
+    a15 = + "123", 
+    a16 = 1 || 0, 
+    a17 = false || true, 
+    a18 = true > 0;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+console.log(a6);
+console.log(a7);
+console.log(a8);
+console.log(a9);
+console.log(a10);
+console.log(a11);
+console.log(a12);
+console.log(a13);
+console.log(a14);
+console.log(a15);
+console.log(a16);
+console.log(a17);
+console.log(a18);
+
+console.log(a1, typeof a1);
+console.log(a2, typeof a2);
+console.log(a3, typeof a3);
+console.log(a4, typeof a4);
+console.log(a5, typeof a5);
+console.log(a6, typeof a6);
+console.log(a7, typeof a7);
+console.log(a8, typeof a8);
+console.log(a9, typeof a9);
+console.log(a10, typeof a10);
+console.log(a11, typeof a11);
+console.log(a12, typeof a12);
+console.log(a13, typeof a13);
+console.log(a14, typeof a14);
+console.log(a15, typeof a15);
+console.log(a16, typeof a16);
+console.log(a17, typeof a17);
+console.log(a18, typeof a18);
+
+
+// Task 3
+
+let height = 23,
+    width = 10,
+    SPryam = height * width;
+
+console.log(height);
+console.log(width);
+console.log(SPryam);
+console.log(SPryam, typeof SPryam);
+
+// Task 4
+
+let volume = 10,
+    VCilindra = volume * a7;
+
+console.log(VCilindra);
+console.log(VCilindra, typeof VCilindra);
+
+
+// Task 5
+
+let r = 5,
+    p = 3.14,
+    SKruga = (r * r) * p;
+
+console.log(SKruga);
+console.log(SKruga, typeof SKruga);
+
+// Task 6
+
+let a = 5,
+    b = 7,
+    h = 10,
+    STrap = (a + b) * h;
+
+console.log(STrap);
+console.log(STrap, typeof STrap);
+
+// Task 7
+
+{
+ let S = 2000000,
+     p = 10,
+     years = 5,
+     Pereplata = ((S * (1 / 100)) * p) * years;
+
+console.log(Pereplata);  
+console.log(Pereplata, typeof Pereplata); 
+}
+
+// Task 8
+
+{
+  let a = 8,
+      b = 3,
+      x =  8 + b - a / 2;
+
+console.log(x);
+}
+
+{
+    let a = 8,
+        b = 3,
+        x =  a - (15 * b) / (b - 6);
+  
+  console.log(x);
+  }
+
+{
+    let a = 8,
+        b = 3,
+        x = 23780 / (3 + a + b);
+  
+  console.log(x);
+}
